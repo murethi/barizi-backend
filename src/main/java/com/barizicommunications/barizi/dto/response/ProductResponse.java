bf1eabec0e8e5841d427ec671a2991298699b4c2
murethi <murethibrian@outlook.com>
2024-03-30 02:39:28 +0300

    feature: product response DTO object

---
+++ src/main/java/com/barizicommunications/barizi/dto/response/ProductResponse.java
@@ -0,0 +1,9 @@
+package com.barizicommunications.barizi.dto.response;
+
+import lombok.Builder;
+
+import java.util.UUID;
+
+@Builder
+public record ProductResponse(UUID id, String name, String description) {
+}
