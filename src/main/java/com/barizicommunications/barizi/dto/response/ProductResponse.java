c3ea41cee957f300f2011ab1f348a46b4a51dadc
murethi <murethibrian@outlook.com>
2024-03-30 02:50:23 +0300

    refactor: add current stock and minimum stock properties

--- src/main/java/com/barizicommunications/barizi/dto/response/ProductResponse.java
+++ src/main/java/com/barizicommunications/barizi/dto/response/ProductResponse.java
@@ -5,5 +5,5 @@ import lombok.Builder;
 import java.util.UUID;
 
 @Builder
-public record ProductResponse(UUID id, String name, String description) {
+public record ProductResponse(UUID id, String name, String description,int currentStock,int minimumStockLevel) {
 }
