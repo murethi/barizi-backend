376a96a24f804b714aec43ca6f6efc7779632a21
murethi <murethibrian@outlook.com>
2024-03-30 04:21:35 +0300

    chore:enable async (application level)

--- src/main/java/com/barizicommunications/barizi/BariziApplication.java
+++ src/main/java/com/barizicommunications/barizi/BariziApplication.java
@@ -3,9 +3,11 @@ package com.barizicommunications.barizi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class BariziApplication {
 
     public static void main(String[] args) {
