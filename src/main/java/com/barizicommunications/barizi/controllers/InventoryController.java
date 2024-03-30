e7e68a0f2e538ff263c6d3e5b3e3afbb748cc8e0
murethi <murethibrian@outlook.com>
2024-03-30 04:21:13 +0300

    feature: inventory controller, used to update stock

---
+++ src/main/java/com/barizicommunications/barizi/controllers/InventoryController.java
@@ -0,0 +1,20 @@
+package com.barizicommunications.barizi.controllers;
+
+import com.barizicommunications.barizi.dto.response.ProductResponse;
+import com.barizicommunications.barizi.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class InventoryController {
+    private InventoryService inventoryService;
+
+    @PutMapping("{id}")
+    public ProductResponse update(@PathVariable UUID id,@RequestParam int quantity){
+        return inventoryService.updateStock(id,quantity);
+    }
+}
