85085d3a0f86d46e30f9e4f9a9a66b379cf12805
murethi <murethibrian@outlook.com>
2024-03-30 03:01:37 +0300

    chore: add documentation

--- src/main/java/com/barizicommunications/barizi/dto/request/ProductRequest.java
+++ src/main/java/com/barizicommunications/barizi/dto/request/ProductRequest.java
@@ -3,5 +3,8 @@ package com.barizicommunications.barizi.dto.request;
 import lombok.Builder;
 
 @Builder
+/**
+ * Object holds request data related to product
+ */
 public record ProductRequest(String name, String description) {
 }
