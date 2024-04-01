00f643964b3a12254e43047768ad16ff3763afe3
murethi <murethibrian@outlook.com>
2024-04-01 16:55:24 +0300

    enhancement: replace java Functional interface with method

--- src/main/java/com/barizicommunications/barizi/mapper/ProductMapper.java
+++ src/main/java/com/barizicommunications/barizi/mapper/ProductMapper.java
@@ -3,15 +3,15 @@ package com.barizicommunications.barizi.mapper;
 import com.barizicommunications.barizi.dto.request.ProductRequest;
 import com.barizicommunications.barizi.dto.response.ProductResponse;
 import com.barizicommunications.barizi.models.Product;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.function.Function;
-
 @Service
+@NoArgsConstructor
 public class ProductMapper {
 
-    public Function<Product, ProductResponse> toDto = product->{
+    public ProductResponse toDto (Product product){
         return ProductResponse.builder()
                 .id(product.getId())
                 .name(product.getName())
@@ -21,7 +21,7 @@ public class ProductMapper {
                 .build();
     };
 
-    public Function<ProductRequest, Product> toEntity = productRequest->{
+    public Product toEntity(ProductRequest productRequest){
         Product product = Product.builder()
                 .build();
         //by using bean utils, I am able to copy all properties from the request object to the entity
